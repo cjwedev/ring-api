81c11df15335f3d85d90e575e174d72523d8319d
rorfan8782 <knhsinbad8782@gmail.com>
2015-06-29 14:00:38 +0200

    Google Location Marker

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'visitors#index'
+  # root 'visitors#index'
   get '/location/:device_id', :to => 'visitors#map', :as => 'location'
 
   namespace :api do
