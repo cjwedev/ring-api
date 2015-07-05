cf933445d4341b952a0339f37e6394b71a254ee0
rorfan8782 <knhsinbad8782@gmail.com>
2015-07-05 05:18:42 +0200

    tesintg code on heroku

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,9 @@ class VisitorsController < ApplicationController
   def map
     @location = Location.where("device_id = ?", params[:device_id]).try(:first)
     puts "*" * 10
-    puts "#{location.longitude}"
+    puts "#{@location.longitude}"
     puts "*" * 10
-    puts "#{location.latitude}"
+    puts "#{@location.latitude}"
   end
 
   def index
